c63fdd2e1dd7d1380282ec052368e65af8cf6670
root <root@DESKTOP-LJOVTP6.localdomain>
2019-04-17 13:44:58 -0500

    updates

--- api/auth/facebook.js
+++ api/auth/facebook.js
@@ -1,32 +1,26 @@
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const db = require('../../database/dbConfig.js');
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const db = require("../../database/dbConfig.js");
 
-passport.use(new FacebookStrategy(
+passport.use(
+  new FacebookStrategy(
     {
-        clientID: 2,
-        clientSecret: "",
-        callbackURL: 'http://localhost:5000/auth/facebook/callback',
-        profileFields: ["id", "emails", "name"]
+      clientID: 162206191387607,
+      clientSecret: "def2f3f1293a76aaaf785dd354c4f9ff",
+      callbackURL: "http://localhost:5000/auth/facebook/callback",
+      profileFields: ["id", "emails", "name"],
     },
     async function(accessToken, refreshToken, profile, done) {
-        try {
-            const user = await db('users').where({ userId: profile.id }).first();
-            console.log('profile', profile);
-            console.log('user', user)
-            if (user) {
-                return done(null, user)
-            } else {
-                const newUser = {
-                    email: profile.emails[0].value,
-                    firstName: profile.name.givenName,
-                    lastName: profile.name.familyName,
-                    username: profile.name.givenName + profile.name.familyName,
-
-                }
-            }
-        } catch (error) {
-            if (error) return done(error, false);    
-        }        
-    }
-))
+      try {
+        const user = await db("users")
+          .where({ userId: profile.id })
+          .first();
+        if (user) {
+          return done(null, user);
+        }
+      } catch (error) {
+        if (error) return done(error, false);
+      }
+    },
+  ),
+);
